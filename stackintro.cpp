10273c65be400051935432b69e5acf472bcdda9e
Arnav <105158586+arnavbee@users.noreply.github.com>
2024-07-28 01:04:57 +0530

    pop push operations

--- stackintro.cpp
+++ stackintro.cpp
@@ -6,5 +6,26 @@ int main() {
     // creation of stack
     stack<int> s;
 
+    s.push(2);
+    s.push(3);
+
+    s.pop();
+
+    cout<<s.top()<<endl;
+
+    s.pop();
+
+    
+
+
+
+    if(s.empty()){
+        cout<<"Stack is empty"<<endl;
+    }
+
+    else {
+        cout<<"Stack is not empty"<<endl;
+    }
+
     return 0;
 }
