ab7bd900d2fc6c1c10af0bac87779975dbd1e844
Arnav <105158586+arnavbee@users.noreply.github.com>
2024-08-23 22:33:20 +0530

    prims algorithm

---
+++ graphs/PrimsAlgorithm.cpp
@@ -0,0 +1,33 @@
+ int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int, int>>> pq;
+        
+        vector<int> vis(V,0);
+        
+        pq.push({0,0});
+        int sum = 0;
+        while(!pq.empty()){
+            auto it = pq.top();
+            pq.pop();
+            
+            int node = it.second;
+            int wt = it.first;
+            
+            if(vis[node] == 1)
+            continue;
+            
+            vis[node] = 1;
+            
+            sum+=wt;
+            
+            for(auto it: adj[node]){
+                int adjNode = it[0];
+                int edW = it[1];
+                
+                if(!vis[adjNode]){
+                    pq.push({edW, adjNode});
+                }
+            }
+        }
+        return sum;
+    }
