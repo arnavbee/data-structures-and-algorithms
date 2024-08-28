bbaddc018bfc704d7afb20f7deac846a4bd5e68f
Arnav <105158586+arnavbee@users.noreply.github.com>
2024-08-28 23:35:15 +0530

    aded comments

--- arrays/container-with-most-water.cpp
+++ arrays/container-with-most-water.cpp
@@ -1,28 +1,32 @@
-  int maxArea(vector<int>& height) {
-        int n = height.size();
+int maxArea(vector<int>& height) {
+    int n = height.size();
 
-        int i = 0;
-        int j = n - 1;
+    int i = 0;
+    int j = n - 1;
 
-        int maxWater = 0;
+    int maxWater = 0;
 
-        while(i<j){
-            int w = j - i;
-            int h = min(height[i], height[j]);
+    // Iterate until the two pointers meet
+    while (i < j) {
+       
+        // Calculate the width of the container formed by the two pointers
+        int w = j - i;
 
-            int area = h * w;
+        // Calculate the height of the container, which is the smaller of the two heights
+        int h = min(height[i], height[j]);
 
-            maxWater = max(maxWater, area);
+        int area = h * w;
 
-            if(height[i] > height[j]){
-                j--;
-            }
+        // Update maxWater if the current area is larger than the previous max
+        maxWater = max(maxWater, area);
 
-            else {
-                i++;
-            }
-
-
+        // Move the pointer pointing to the shorter height inward, to try and find a taller container
+        if (height[i] > height[j]) {
+            j--;
+        } else {
+            i++;
         }
-        return maxWater;
     }
+
+    return maxWater;
+}
