375bdb3b06e26aeb78ae6a80210829916adc5b72
Arnav <105158586+arnavbee@users.noreply.github.com>
2024-09-06 13:26:04 +0530

    counting bits

---
+++ binary/counting-bits.cpp
@@ -0,0 +1,21 @@
+    vector<int> result(n + 1);
+
+        if(n == 0)
+        return result;
+
+        result[0] = 0;
+
+        for(int i = 0; i <=n; i++){
+            if(i%2 != 0){
+                result[i] = result[i/2] + 1;
+            }
+
+            else {
+                result[i] = result[i/2];
+            }
+
+        }
+
+        return result;
+        
+    }
