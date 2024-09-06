5645329599915be3bcff654eb9e25484018e43ea
Arnav <105158586+arnavbee@users.noreply.github.com>
2024-09-06 12:58:30 +0530

    binary new - adding number of 1 bits

---
+++ binary/number-of-1-bits.cpp
@@ -0,0 +1,11 @@
+  int hammingWeight(int n) {
+        int count = 0;
+
+        while(n != 0){
+            n = n & (n - 1);
+            count++;
+        }
+
+        return count;
+      
+    }
