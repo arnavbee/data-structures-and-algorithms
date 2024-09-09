17d01fd2a33dee8dded558c02f5cdbbd7c3399d9
Arnav <105158586+arnavbee@users.noreply.github.com>
2024-09-09 12:27:05 +0530

    invert a binary tree

---
+++ binary-tree/invert-a-binary-tree.cpp
@@ -0,0 +1,17 @@
+    TreeNode* invertTree(TreeNode* root) {
+        
+        if(root == NULL)
+        return NULL;
+
+        invertTree(root -> left);
+        invertTree(root -> right);
+
+        TreeNode* temp = root -> left;
+        root -> left = root -> right;
+        root -> right = temp;
+
+        return root;
+
+
+
+    }
